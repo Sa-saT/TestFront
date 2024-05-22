dbd8d7ac4e31ccf0d35c2f42b5035b582b3031e5
Sa-saT <sasa.is.music@gmail.com>
2024-05-23 00:04:55 +0900

    add 20240523

--- nuxt-app/layouts/default.vue
+++ nuxt-app/layouts/default.vue
@@ -9,5 +9,13 @@
         <NuxtLink to="/browse" class="text-white hover:text-teal-300">Browse</NuxtLink>
       </div>
     </nav>
+    <slot />
+    <footer class="p-6 flex flex-warp items-center justify-between bg-gray-900">
+      <p class="text-gray-300">Copyright (c) 2023 - Djob</p>
+      <div class="flex mt-6 md:mt-0 items-center space-x-4">
+        <NuxtLink to="/login" class="py-4 px-6 bg-teal-900 hover:bg-teal-700 text-white rounded-xl">Log in</NuxtLink>
+        <NuxtLink to="/signup" class="py-4 px-6 bg-teal-900 hover:bg-teal-700 text-white rounded-xl">Sign up</NuxtLink>
+      </div>
+    </footer>
   </div>
 </template>
