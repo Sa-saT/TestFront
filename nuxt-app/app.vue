3a4719d7fc8e689af470620581c8f0d67541cff3
Sa-saT <sasa.is.music@gmail.com>
2024-07-05 00:49:24 +0900

    add 240704

--- nuxt-app/app.vue
+++ nuxt-app/app.vue
@@ -10,6 +10,16 @@ useState<Map<number, Member>>(
   }
 )
 </script> -->
+<script setup>
+import { onMounted } from 'vue'
+import { useUserStore } from '@/stores/user'
+
+const userStore = useUserStore()
+
+onMounted(() => {
+  userStore.initStore()
+}) 
+</script>
 <template>
   <div>
     <NuxtLayout>
--- nuxt-app/pages/myjobs.vue
+++ nuxt-app/pages/myjobs.vue
@@ -1,10 +1,45 @@
+<script setup>
+import { onMounted } from 'vue'
+import { useUserStore } from '@/stores/user'
+
+const userStore = useUserStore()
+const router = useRouter()
+let jobs = ref()
+
+onMounted(() => {
+    if (!userStore.user.isAuthenticated){
+        routerKey.push('/login')
+    } else {
+        getJobs()
+    }
+})
+async function getJobs(){
+    await $fetch('http://127.0.0.1:8000/api/v1/jobs/my', {
+        headers:{
+            'Authorization': 'token ' + userStore.user.token,
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => {
+        jobs.value = response
+    })
+    .catch(error => {
+        console.log('error', error)
+    })
+}
+</script>
+
 <template>
     <div>
         <div class="py-10 px-6">
             <h1 class="mb-6 text-2xl">My Jobs</h1>
             <div class="space-y-4">
-                <Job :my="true"/>
-				
+                <Job
+                    v-for="job in jobs"
+                    :key="job.id"
+                    :job="job"
+                    :my="true"
+                />	
 			</div>
         </div>
     </div>
